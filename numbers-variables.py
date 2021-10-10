d49abca7bb138b1080dbd1d1beb8545e611f0711
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-10 19:13:47 -0300

    Create numbers-variables.py

---
+++ numbers-variables.py
@@ -0,0 +1,13 @@
+# VARIABLES NUMERICAS
+# En Python podemos almacenar 3 tipos de valores numéricos. 
+# 1. Integer (int)
+# 2. Float (float)
+# 3. Complex (complex)
+
+a = 1 # int
+b = 1.5 # float
+c = 1j # complex
+
+print(type(a)) # Mostrará que la clase es int
+print(type(b)) # Mostrará que la clase es float
+print(type(c)) # Mostrará que la clase es complex
