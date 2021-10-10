9fb7257874bb698d710657ce8dec16376ddf3870
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-10 10:53:41 -0300

    Create creating-variables.py

---
+++ creating-variables.py
@@ -0,0 +1,8 @@
+# CREAR VARIABLES
+# Las variables en Python pueden almacenar los datos que le sean asignados.
+
+name = "Koche" # Creo la variable name y le asigno el valor "Koche" (str).
+age = 35 # Creo la variable age y le asigno el valor 35 (int).
+print(name) # Le pido que imprima la variable name.
+print(age) # Le pido que imprima la variable age.
+input() # Sale del programa.
