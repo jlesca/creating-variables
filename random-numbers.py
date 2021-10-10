c168489be8f97a8bd2ecac225dcc24187e5571af
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-10 19:50:55 -0300

    Create random-numbers.py

---
+++ random-numbers.py
@@ -0,0 +1,8 @@
+# CREAR NUMEROS AL AZAR
+# Python ha creado un módulo llamado 'random' que nos permite crear un número al azar dentro de un rango establecido.
+# Por ejemplo, le pediremos que cree un número al azar entre 1 y 9.
+# En Python Para llamar un módulo debemos hacerlo mediante la keyword 'import'
+
+import random # Importa le módulo random.
+
+print(random.randrange(1, 10)) # Imprime un número al azar dentro del rango establecido. En este caso entre 1 y 9 (10 no se cuenta).
