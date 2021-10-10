83bd8d44687ba853f4f871b98a884fa6c09c8833
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-10 11:22:05 -0300

    Create datatype-variables.py

---
+++ datatype-variables.py
@@ -0,0 +1,12 @@
+# TIPO DE DATOS EN LAS VARIABLES
+# Los datos que guardemos en nuestras variables pueden ser:
+# Texto, númericos, decimales, verdadero o falso, entre otros.
+# Python los identifica de manera abreviada por sus definiciones en inglés.
+
+# Podemos especificar que tipo de dato queremos que sea el valor de nuestra variable.
+
+string = str(5) # La variable string valdrá = "5"
+number = int(5) # La variable number valdrá = 5
+decimal = float(5) # La variable decimal valdrá = "5.0"
+print(string, number, decimal) # Le pido que muestre las variables.
+input()
