e4ef018dedf8bb5c33f85d75e060d3874ea44678
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-10 11:39:57 -0300

    Create multiple-variables.py

---
+++ multiple-variables.py
@@ -0,0 +1,10 @@
+# MULTIPLES VALORES EN MULTIPLES VARIABLES
+# Python nos permite asignar múltiples valores y múltipleas variables en una sola línea.
+
+a, b, c = 'red', 'green', 'blue' # Creo las variables y le asigno los valores a cada una, separando con una coma.
+print(a) # Muestro la variable
+print(b) # Muestro la variable
+print(c) # Muestro la variable
+
+# También podríamos mostrar los valores del siguiente modo:
+print(a, b, c) # Muestro las variables en una sola línea.
